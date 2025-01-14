21930cb231f3bab96dd74b9522bfe54745e2bbe5
Abdullah20040101 <abdullahudul88@gmail.com>
2025-01-14 10:32:55 +0800

    Add files via upload

---
+++ tugas2pbo2/src/tugas2pbo2/Tugas2pbo2.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package tugas2pbo2;
+import crudDB.crudDB;
+import javax.swing.JOptionPane;
+import gui.utama;
+/**
+ *
+ * @author asus
+ */
+public class Tugas2pbo2 {
+    public static void main(String[] args) {
+        
+        new utama().setVisible(true);
+
+   
+    }
+    
+}
